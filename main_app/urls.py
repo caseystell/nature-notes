bb70bd53847c5d1efef84d69b71c617a1bfa8ec8
caseystell <cleigh14@hotmail.com>
2023-08-14 14:46:46 -0600

    CSS updates and Amenity functionality fixes

--- main_app/urls.py
+++ main_app/urls.py
@@ -15,7 +15,6 @@ urlpatterns = [
     path('amenities/', views.AmenityList.as_view(), name='amenities_index'),
     path('amenities/<int:pk>', views.AmenityDetail.as_view(), name='amenities_detail'),
     path('amenities/create/', views.AmenityCreate.as_view(), name='amenities_create'),
-    path('amenities/<int:pk>/update/', views.AmenityUpdate.as_view(), name='amenities_update'),
     path('amenities/<int:pk>/delete/', views.AmenityDelete.as_view(), name='amenities_delete'),
     path('accounts/signup/', views.signup, name='signup'),
 
