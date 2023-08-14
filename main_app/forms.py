0f9cf03b08303d809b64d545577d4ca57cce8c7c
username <stever0407@yahoo.com>
2023-08-14 09:49:03 -0700

    First Commit

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,12 @@
 from django.forms import ModelForm
-from .models import Trail
+from .models import Trail, Amenity 
 
 class TrailForm(ModelForm):
   class Meta:
     model = Trail
     fields = ['name', 'length', 'description']
+
+class AmenityForm(ModelForm):
+  class Meta:
+    model = Amenity
+    fields= ['name']
